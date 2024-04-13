5f91796b6f32c99a0eef64d0034c75d8c3c1182e
Amr Attyah <amratt5@gmail.com>
2024-04-13 15:39:37 -0400

    Making the car move forward using a different way

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Making the car move forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward);
     }
 }
